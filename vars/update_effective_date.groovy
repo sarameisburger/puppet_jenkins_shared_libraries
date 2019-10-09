cacbb9e03957a43731a4ef7f975a7e5b05725e5f
Nick Burgan-Illig <nick.burgan-illig@puppet.com>
2019-10-09 20:21:46 +0000

    (PE-27344) Add update_effective_date function for updating EFFECTIVE_DATE in shim
    This creates a function that takes an effective date string, a pe-installer-shim branch, and a ticket identifier, and will update the EFFECTIVE_DATE variable in the shim.  It verifies that the date given is in the correct format, clones and checks out the given branch of pe-installer-shim, modifies the EFFECTIVE_DATE variable, and pushes a commit to origin with that change.

---
+++ vars/update_effective_date.groovy
@@ -0,0 +1,27 @@
+def call(String effectiveDate, String branch, String ticket) {
+
+  if (effectiveDate =~ '^20[0-9]{2}[.](0[1-9]|1[0-2])[.](0[1-9]|[12][0-9]|3[01])$') {
+    println "${effectiveDate} is a valid EFFECTIVE_DATE"
+  } else {
+    println "${effectiveDate} is an invalid EFFECTIVE_DATE. Must be in the format YYYY.MM.DD with valid dates."
+    throw new Exception("Invalid effective date")
+  }
+
+  sh """
+  #!/usr/bin/env bash
+
+  rm -rf ./pe-installer-shim
+  git clone git@github.com:puppetlabs/pe-installer-shim ./pe-installer-shim
+  cd pe-installer-shim
+  git checkout \"${branch}\"
+  sed -i \"/readonly EFFECTIVE_DATE=.*/c\\readonly EFFECTIVE_DATE='${effectiveDate}'\" ./puppet-enterprise-installer
+  git add puppet-enterprise-installer
+
+  git commit -m \"(${ticket}) Update EFFECTIVE_DATE to ${effectiveDate}\"
+  git push origin \"${branch}\"
+
+  # Cleanup
+  cd ..
+  rm -rf pe-installer-shim
+  """
+}
