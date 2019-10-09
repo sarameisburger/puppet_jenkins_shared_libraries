a84b16dd89af9e9129fece7ce45dd8fc835c5041
Nick Burgan-Illig <nick.burgan-illig@puppet.com>
2019-10-09 21:42:15 +0000

    fix2

--- vars/update_effective_date.groovy
+++ vars/update_effective_date.groovy
@@ -14,7 +14,7 @@ def call(String effectiveDate, String branch, String ticket) {
   git clone git@github.com:puppetlabs/pe-installer-shim ./pe-installer-shim
   cd pe-installer-shim
   git checkout "${branch}"
-  sed -i "/readonly EFFECTIVE_DATE=.*/c\readonly EFFECTIVE_DATE='${effectiveDate}'" ./puppet-enterprise-installer
+  sed -i "/readonly EFFECTIVE_DATE=.*/c\\readonly EFFECTIVE_DATE='${effectiveDate}'" ./puppet-enterprise-installer
   git add puppet-enterprise-installer
 
   git commit -m "(${ticket}) Update EFFECTIVE_DATE to ${effectiveDate}"
