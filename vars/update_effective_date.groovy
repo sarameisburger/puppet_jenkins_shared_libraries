d6c37b21883e9db74468a0fbe4cf0b9739aa3dba
Nick Burgan-Illig <nick.burgan-illig@puppet.com>
2019-10-09 21:39:28 +0000

    fix

--- vars/update_effective_date.groovy
+++ vars/update_effective_date.groovy
@@ -1,6 +1,6 @@
 def call(String effectiveDate, String branch, String ticket) {
 
-  if (effectiveDate =~ '^20[0-9]{2}\.(0[1-9]|1[0-2])\.(0[1-9]|[12][0-9]|3[01])$') {
+  if (effectiveDate =~ '^20[0-9]{2}[.](0[1-9]|1[0-2])[.](0[1-9]|[12][0-9]|3[01])$') {
     println "${effectiveDate} is a valid EFFECTIVE_DATE"
   } else {
     println "${effectiveDate} is an invalid EFFECTIVE_DATE. Must be in the format YYYY.MM.DD with valid dates."
