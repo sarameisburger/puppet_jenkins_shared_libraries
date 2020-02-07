174bf3645a447bb2162151e2d98b0d6a53b4f3da
Adrian Towery <Adrian.Towery@gmail.com>
2020-02-06 16:07:00 -0800

    (maint) Add credentials wrapper to node work

--- vars/init_release_job_creation.groovy
+++ vars/init_release_job_creation.groovy
@@ -8,8 +8,10 @@ def call(String version) {
   }
   //Execute bash script, catch and print output and errors
   node('worker') {
-    sh "curl -O https://raw.githubusercontent.com/puppetlabs/puppet_jenkins_shared_libraries/master/vars/bash/init_release_job_creation.sh"
-    sh "chmod +x init_release_job_creation.sh"
-    sh "./init_release_job_creation.sh $version"
+    withCredentials([string(credentialsId: '23127746-57ff-4112-82c2-ee00eb403718', variable: 'GITHUB_TOKEN')]) {
+      sh "curl -O https://raw.githubusercontent.com/puppetlabs/puppet_jenkins_shared_libraries/master/vars/bash/init_release_job_creation.sh"
+      sh "chmod +x init_release_job_creation.sh"
+      sh "./init_release_job_creation.sh $version"
+    }
   }
 }
