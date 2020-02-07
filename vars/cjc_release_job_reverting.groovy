01c4efd16e272b7a9e4f1f29e768df3ede6066f2
Christopher Thorn <christopher.thorn@puppetlabs.com>
2020-02-07 14:47:02 -0800

    (maint) Revert release jobs

---
+++ vars/cjc_release_job_reverting.groovy
@@ -0,0 +1,17 @@
+def call(String version) {
+
+  if (version =~ '^20[0-9]{2}[.]([0-9]*)[.]([0-9]*)$') {
+    println "${version} is a valid version"
+  } else {
+    println "${version} is an invalid version"
+    throw new Exception("Invalid version")
+  }
+  //Execute bash script, catch and print output and errors
+  node('worker') {
+    withCredentials([string(credentialsId: 'githubtoken', variable: 'GITHUB_TOKEN')]) {
+      sh "curl -O https://raw.githubusercontent.com/puppetlabs/puppet_jenkins_shared_libraries/revert_release_jobs_branch/vars/bash/release_job_reverts.sh"
+      sh "chmod +x release_job_reverts.sh"
+      sh "./release_job_reverts.sh $version"
+    }
+  }
+}
